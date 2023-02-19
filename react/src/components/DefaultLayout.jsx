cc1b2a807a92b91c6d4ba5ce65cfea63a11b71ef
satuhin95 <saifcm95@gmail.com>
2023-02-19 15:24:56 +0600

    some ui update

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../context/ContextProvider'
 
 export default function DefaultLayout() {
+  const {user,token} = useStateContext();
+  if(!token){
+    return <Navigate to="/login"/>
+  }
   return (
-    <div>
-        <Outlet/>
+    <div id='defaultLayout'>
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+      <div className='content'>
+        <header>
+          <div>
+            Header
+          </div>
+          <div>
+            {user.name}
+            <a className='btn-logout '>LogOut</a>
+          </div>
+        </header>
+        <main>
+         <Outlet/>
+        </main>
+      </div>
     </div>
   )
 }
--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useStateContext } from '../context/ContextProvider'
 
 export default function GuestLayout() {
+  const {token} = useStateContext();
+  if(token){
+    return <Navigate to="/" />
+  }
+  // debugger;
   return (
     <div>
         <div>
-            Gest User Only
-        <Outlet/>
+          <Outlet/>
         </div>
     </div>
   )
